a06462fdbfd98666bdb97a853ab3e56e547f4af4
Yusuke MURAOKA <yusuke@jbking.org>
2022-10-15 12:49:35 +0900

    add link list -> detail

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+test-local:
+	PYTHONPATH=$(PWD) poetry run st --pre-run setup_test run -vv --checks all --app=main:app /openapi.json
+
 test:
-	PYTHONPATH=$(PWD) poetry run st run --checks all --app=main:app /openapi.json
+	PYTHONPATH=$(PWD) poetry run st --pre-run setup_test run -vv --checks all http://localhost:8000/openapi.json
---
+++ setup_test.py
@@ -0,0 +1,16 @@
+import schemathesis
+
+
+@schemathesis.hooks.register
+def after_load_schema(
+    context: schemathesis.hooks.HookContext,
+    schema: schemathesis.schemas.BaseSchema,
+) -> None:
+    schema.add_link(
+        source=schema["/items/"]["GET"],
+        target=schema["/items/{item_id}"]["GET"],
+        status_code=200,
+        # https://github.com/schemathesis/schemathesis/issues/900
+        # only support hard coded curretly
+        parameters={"item_id": "$response.body#/items/1/item_id"},
+    )
